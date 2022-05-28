64d4260fe5e3524e9f7e4d516ee4e8ffd10e8190
Bradley Ting <bting@goshippo.com>
2022-05-28 14:59:54 -0700

    Add cors

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, Response, request
+from flask_cors import CORS, cross_origin
 from db import get_ducky, init_db, update_ducky_location, get_all_duckies
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 class BadRequestException(Exception):
@@ -10,12 +13,14 @@ class BadRequestException(Exception):
 
 
 @app.route('/')
+@cross_origin()
 def get_duckies():
     duckies_list = get_all_duckies()
     return json.dumps(duckies_list, default=str)
 
 
 @app.route('/location', methods=['POST'])
+@cross_origin()
 def update_location():
     try:
         ducky_id = request.json['ducky_id']
@@ -33,12 +38,15 @@ def update_location():
 
 
 @app.route('/travel')
+@cross_origin()
 def move_ducky_1():
     ducky = get_ducky(1)
     location_id = ducky[1]
     new_location = 1 if location_id == 2 else 2
     update_ducky_location(ducky_id=1, location_id=new_location)
-    return Response(status=203)
+
+    duckies_list = get_all_duckies()
+    return json.dumps(duckies_list, default=str)
 
 
 if __name__ == '__main__':
