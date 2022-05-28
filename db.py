15f4f243cbb433ef9d6588418478bca87f4fb0e2
Bradley Ting <bting@goshippo.com>
2022-05-28 12:43:52 -0700

    Add get and post endpoints'

--- db.py
+++ db.py
@@ -1,39 +1,54 @@
 import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
-DATABASE_URL = 'postgres://snfzjbzottankj:863d78821e6ea8be9a60abe9fd829eed951cbcf100707bb3861ea0667c7df423@ec2-44-196-174-238.compute-1.amazonaws.com:5432/d8fitsasagpcfa'
+DATABASE_URL = os.environ['DATABASE_URL']
+
 
 def get_db_conn():
-  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-  return conn
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    return conn
+
 
 def init_db():
-  conn = get_db_conn()
-  cur = conn.cursor()
+    conn = get_db_conn()
+    cur = conn.cursor()
 
-  cur.execute('CREATE TABLE IF NOT EXISTS owner (id serial PRIMARY KEY,'
-                                   'name varchar (150) NOT NULL,'
-                                   'ip_address varchar (150),'
-                                   'object_created date DEFAULT CURRENT_TIMESTAMP);'
-                                   )
+    cur.execute('CREATE TABLE IF NOT EXISTS owner (id serial PRIMARY KEY,'
+                'name varchar (150) NOT NULL,'
+                'ip_address varchar (150),'
+                'object_created date DEFAULT CURRENT_TIMESTAMP);'
+                )
 
-  cur.execute('CREATE TABLE IF NOT EXISTS ducky (id serial PRIMARY KEY,'
-                                   'name varchar (150) NOT NULL,'
-                                   'owner_id int REFERENCES owner(id) NOT NULL,'
-                                   'location_id int REFERENCES owner(id) NOT NULL,'
-                                   'object_created date DEFAULT CURRENT_TIMESTAMP);'
-                                   )
+    cur.execute('CREATE TABLE IF NOT EXISTS ducky (id serial PRIMARY KEY,'
+                'name varchar (150) NOT NULL,'
+                'owner_id int REFERENCES owner(id) NOT NULL,'
+                'location_id int REFERENCES owner(id) NOT NULL,'
+                'object_created date DEFAULT CURRENT_TIMESTAMP);'
+                )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
 
 
-  conn.commit()
+def get_all_duckies():
+    conn = get_db_conn()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    query_sql = "SELECT * FROM ducky"
+    cur.execute(query_sql)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
 
-  cur.close()
-  conn.close()
 
 def update_ducky_location(ducky_id, location_id):
-  conn = get_db_conn()
-  cur = conn.cursor()
-  cur.execute("UPDATE ducky SET location_id = %s WHERE id = %s", (location_id, ducky_id))
-  conn.commit()
-  cur.close()
-  conn.close()
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute("UPDATE ducky SET location_id = %s WHERE id = %s",
+                (location_id, ducky_id))
+    conn.commit()
+    cur.close()
+    conn.close()
--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from flask import Flask
-from db import init_db, update_ducky_location
+from flask import Flask, Response, request
+from db import init_db, update_ducky_location, get_all_duckies
+import json
 
 app = Flask(__name__)
 
+
+class BadRequestException(Exception):
+    pass
+
+
 @app.route('/')
-def hello_world():
-    update_ducky_location(1, 1)
-    return 'Updated ducky location!'
+def get_duckies():
+    duckies_list = get_all_duckies()
+    return json.dumps(duckies_list, default=str)
+
+
+@app.route('/location', methods=['POST'])
+def update_location():
+    try:
+        ducky_id = request.json['ducky_id']
+        location_id = request.json['location_id']
+        if not isinstance(ducky_id, int) or not isinstance(location_id, int):
+            raise BadRequestException
+    except KeyError:
+        return Response(status=400)
+    except BadRequestException:
+        return Response(status=400)
+
+    update_ducky_location(ducky_id=ducky_id, location_id=location_id)
+
+    return Response(status=201)
+
 
 if __name__ == '__main__':
     app.run()
