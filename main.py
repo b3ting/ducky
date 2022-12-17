8d1626611bfd07e6496ba1203adaed4cfa6ab5b9
bradleyting <bradley.ting@samsara.com>
2022-12-17 12:03:56 -0800

    try fix cors

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 class BadRequestException(Exception):
     pass
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 @app.route('/')
 @cross_origin()
